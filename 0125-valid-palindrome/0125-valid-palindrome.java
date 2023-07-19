d491b17e4ca65e29b3b7a186db57c52c056337b2
Wahyu <wahyuivanmahendra@gmail.com>
2023-07-19 11:14:12 +0800

    Time: 3 ms (97.90%), Space: 42.1 MB (76.96%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while(start <= end){
+            char charStart = s.charAt(start);
+            char charEnd = s.charAt(end);
+
+            if(!Character.isLetterOrDigit(charStart)){
+                start++;
+            }else if(!Character.isLetterOrDigit(charEnd)){
+                end--;
+            }else {
+                if(Character.toLowerCase(charStart) != Character.toLowerCase(charEnd)){
+                    return false;
+                }
+                start++;
+                end--;
+            }
+        }
+
+        return true;
+    }
+}
